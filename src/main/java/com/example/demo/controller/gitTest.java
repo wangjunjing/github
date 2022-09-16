d0ad507ad50ed6cc350a2e047eadf77a68a46e9b
Layne <wang@qq.com>
2022-09-16 10:11:32 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/gitTest.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.processing.SupportedAnnotationTypes;
+@Slf4j
+@Controller
+public class gitTest {
+
+    @Test
+    public void git(){
+        log.info("你好git");
+    }
+}
